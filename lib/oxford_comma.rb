b2c5f904429aec1cb88b5e2bbe5e3ea78a2b1737
Jonathan Block <blockjonathan29@gmail.com>
2021-01-15 12:00:46 -0500

    commit tree 35f3b1c55038aecda28b3103ea99d7ef90adfc7a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length < 2 
+     array.join
+  elsif array.length == 2
+    last_element = array.pop
+    array << "and" + " #{last_element}"  
+    array.join(" ")
+  else
+    last_element = array.pop
+    array << "and" + " #{last_element}"
+    array.join(", ") 
+  end
 end
